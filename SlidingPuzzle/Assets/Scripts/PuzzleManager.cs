206843bfd4a30027a10d15aebdc003436aef25a8
jlima01 <jonatasflima01@gmail.com>
2022-01-23 15:08:19 -0300

    Script edited!

--- SlidingPuzzle/Assets/Scripts/PuzzleManager.cs
+++ SlidingPuzzle/Assets/Scripts/PuzzleManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] puzzlePieces;
+
     void Start()
     {
-        
+        puzzlePieces = GameObject.FindGameObjectsWithTag("Puzzle");
     }
 
     // Update is called once per frame
