b3c1ab40d863e8b1838b8c3c5a8fe11239a04bf3
jlima01 <jonatasflima01@gmail.com>
2022-01-23 20:53:00 -0300

    Update PuzzleManager.cs

--- SlidingPuzzle/Assets/Scripts/PuzzleManager.cs
+++ SlidingPuzzle/Assets/Scripts/PuzzleManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    public GameObject[] Slots;
+    public GameObject[] slots;
+    public List <Vector3> position;
     void Start()
     {
-        Slots = GameObject.FindGameObjectsWithTag("Slot");
+        slots = GameObject.FindGameObjectsWithTag("Slot");
+        CheckPosition();
     }
     void Update()
     {
         
+    }
+    void CheckPosition()
+    {
+        for(int i = 0; i < slots.Length; i++)
+        {
+            position.Add(slots[i].transform.position);
+        }
     }
 }
