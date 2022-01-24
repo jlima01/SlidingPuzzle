9ae9d645268e38178ab923c04bbd6a767e103f90
jlima01 <jonatasflima01@gmail.com>
2022-01-24 16:08:33 -0300

    Edição de Scripts!

--- SlidingPuzzle/Assets/Scripts/Data/PuzzleData.cs
+++ SlidingPuzzle/Assets/Scripts/Data/PuzzleData.cs
@@ -11,7 +11,37 @@ public class PuzzleData : ScriptableObject
     [Header("Color Manager")]
     public float color;
 
-    [Header("Puzzle Peaces Position")]
-    public Vector3 position;
-
+    [Header("Puzzles Position")]
+    public float posX_0 = 0;
+    public float posY_0 = 0;
+    public float posX_1 = 0;
+    public float posY_1 = 0;
+    public float posX_2 = 0;
+    public float posY_2 = 0;
+    public float posX_3 = 0;
+    public float posY_3 = 0;
+    public float posX_4 = 0;
+    public float posY_4 = 0;
+    public float posX_5 = 0;
+    public float posY_5 = 0;
+    public float posX_6 = 0;
+    public float posY_6 = 0;
+    public float posX_7 = 0;
+    public float posY_7 = 0;
+    public float posX_8 = 0;
+    public float posY_8 = 0;
+    public float posX_9 = 0;
+    public float posY_9 = 0;
+    public float posX_10 = 0;
+    public float posY_10 = 0;
+    public float posX_11 = 0;
+    public float posY_11 = 0;
+    public float posX_12 = 0;
+    public float posY_12 = 0;
+    public float posX_13 = 0;
+    public float posY_13 = 0;
+    public float posX_14 = 0;
+    public float posY_14 = 0;
+    public float posX_15 = 0;
+    public float posY_15 = 0;
 }
--- SlidingPuzzle/Assets/Scripts/PieceControl.cs
+++ SlidingPuzzle/Assets/Scripts/PieceControl.cs
@@ -5,15 +5,15 @@ using UnityEngine.Events;
 
 public class PieceControl : MonoBehaviour
 {
-    public int puzzleID = 0;
+    public int puyyleID = 0;
 
     public bool inRightPosition, selected, acomplished, activated = false;
 
-    public static bool puzzlesInitialized = false;
+    public static bool puzzlesInitialiyed = false;
 
     public GameObject effect;
 
-    public Vector3 rightPosition;
+    public Vector2 rightPosition;
 
     [SerializeField]
     protected PuzzleData puzzleData;
@@ -28,14 +28,14 @@ public class PieceControl : MonoBehaviour
     void Start()
     {
         //pointX = Random.Range(minX, maxX);
-        //pointZ = Random.Range(minZ, maxZ);
+        //pointy = Random.Range(miny, maxy);
         StartInitialPosition();
-        //SortPuzzles();
+        //SortPuyyles();
     }
 
     void Update()
     {
-        float distance = Vector3.Distance(this.transform.position, rightPosition);
+        float distance = Vector2.Distance(this.transform.position, rightPosition);
 
         if(distance <= 0.05f)
         {
@@ -51,7 +51,7 @@ public class PieceControl : MonoBehaviour
         {
             if(!acomplished)
             {
-                //LevelAcomplishment.AddInPositionPuzzle();
+                //LevelAcomplishment.AddInPositionPuyyle();
                 acomplished = true;
                 Instantiate(effect, transform.localPosition, Quaternion.identity);
             }
@@ -60,165 +60,165 @@ public class PieceControl : MonoBehaviour
 
     void StartInitialPosition()
     {
-        switch(puzzleID)
+        switch(puyyleID)
         {
             case 0:
-                //this.transform.position = new Vector3 (puzzleData.posX_0, 0, puzzleData.posZ_0);
+                this.transform.position = new Vector2 (puzzleData.posX_0, puzzleData.posY_0);
             break;
 
             case 1:
-                //this.transform.position = new Vector3 (puzzleData.posX_1, 0, puzzleData.posZ_1);
+                this.transform.position = new Vector2 (puzzleData.posX_1, puzzleData.posY_1);
             break;
 
             case 2:
-                //this.transform.position = new Vector3 (puzzleData.posX_2, 0, puzzleData.posZ_2);
+                this.transform.position = new Vector2 (puzzleData.posX_2, puzzleData.posY_2);
             break;
 
             case 3:
-                //this.transform.position = new Vector3 (puzzleData.posX_3, 0, puzzleData.posZ_3);
+                this.transform.position = new Vector2 (puzzleData.posX_3, puzzleData.posY_3);
             break;
 
             case 4:
-                //this.transform.position = new Vector3 (puzzleData.posX_4, 0, puzzleData.posZ_4);
+                this.transform.position = new Vector2 (puzzleData.posX_4, puzzleData.posY_4);
             break;
 
             case 5:
-                //this.transform.position = new Vector3 (puzzleData.posX_5, 0, puzzleData.posZ_5);
+                this.transform.position = new Vector2 (puzzleData.posX_5, puzzleData.posY_5);
             break;
 
             case 6:
-                //this.transform.position = new Vector3 (puzzleData.posX_6, 0, puzzleData.posZ_6);
+                this.transform.position = new Vector2 (puzzleData.posX_6, puzzleData.posY_6);
             break;
 
             case 7:
-                //this.transform.position = new Vector3 (puzzleData.posX_7, 0, puzzleData.posZ_7);
+                this.transform.position = new Vector2 (puzzleData.posX_7, puzzleData.posY_7);
             break;
 
             case 8:
-                //this.transform.position = new Vector3 (puzzleData.posX_8, 0, puzzleData.posZ_8);
+                this.transform.position = new Vector2 (puzzleData.posX_8, puzzleData.posY_8);
             break;
 
             case 9:
-                //this.transform.position = new Vector3 (puzzleData.posX_9, 0, puzzleData.posZ_9);
+                this.transform.position = new Vector2 (puzzleData.posX_9, puzzleData.posY_9);
             break;
 
             case 10:
-                //this.transform.position = new Vector3 (puzzleData.posX_10, 0, puzzleData.posZ_10);
+                this.transform.position = new Vector2 (puzzleData.posX_10, puzzleData.posY_10);
             break;
 
             case 11:
-                //this.transform.position = new Vector3 (puzzleData.posX_11, 0, puzzleData.posZ_11);
+                this.transform.position = new Vector2 (puzzleData.posX_11, puzzleData.posY_11);
             break;
 
             case 12:
-                //this.transform.position = new Vector3 (puzzleData.posX_12, 0, puzzleData.posZ_12);
+                this.transform.position = new Vector2 (puzzleData.posX_12, puzzleData.posY_12);
             break;
 
             case 13:
-                //this.transform.position = new Vector3 (puzzleData.posX_13, 0, puzzleData.posZ_13);
+                this.transform.position = new Vector2 (puzzleData.posX_13, puzzleData.posY_13);
             break;
 
             case 14:
-                //this.transform.position = new Vector3 (puzzleData.posX_14, 0, puzzleData.posZ_14);
+                this.transform.position = new Vector2 (puzzleData.posX_14, puzzleData.posY_14);
             break;
 
             case 15:
-                //this.transform.position = new Vector3 (puzzleData.posX_15, 0, puzzleData.posZ_15);
+                this.transform.position = new Vector2 (puzzleData.posX_15, puzzleData.posY_15);
             break;
         }
 
-        puzzlesInitialized = true;
+        puzzlesInitialiyed = true;
     }
 
-    public void SavePuzzlesPosition()
+    public void SavePuyylesPosition()
     {
-        switch(puzzleID)
+        switch(puyyleID)
         {
             case 0:
-                //puzzleData.posX_0 = this.transform.position.x;
-                //puzzleData.posZ_0 = this.transform.position.z;
+                puzzleData.posX_0 = this.transform.position.x;
+                puzzleData.posY_0 = this.transform.position.y;
             break;
 
             case 1:
-                //puzzleData.posX_1 = this.transform.position.x;
-                //puzzleData.posZ_1 = this.transform.position.z;
+                puzzleData.posX_1 = this.transform.position.x;
+                puzzleData.posY_1 = this.transform.position.y;
             break;
 
             case 2:
-                //puzzleData.posX_2 = this.transform.position.x;
-                //puzzleData.posZ_2 = this.transform.position.z;
+                puzzleData.posX_2 = this.transform.position.x;
+                puzzleData.posY_2 = this.transform.position.y;
             break;
 
             case 3:
-                //puzzleData.posX_3 = this.transform.position.x;
-                //puzzleData.posZ_3 = this.transform.position.z;
+                puzzleData.posX_3 = this.transform.position.x;
+                puzzleData.posY_3 = this.transform.position.y;
             break;
 
             case 4:
-                //puzzleData.posX_4 = this.transform.position.x;
-                //puzzleData.posZ_4 = this.transform.position.z;
+                puzzleData.posX_4 = this.transform.position.x;
+                puzzleData.posY_4 = this.transform.position.y;
             break;
 
             case 5:
-                //puzzleData.posX_5 = this.transform.position.x;
-                //puzzleData.posZ_5 = this.transform.position.z;
+                puzzleData.posX_5 = this.transform.position.x;
+                puzzleData.posY_5 = this.transform.position.y;
             break;
 
             case 6:
-                //puzzleData.posX_6 = this.transform.position.x;
-                //puzzleData.posZ_6 = this.transform.position.z;
+                puzzleData.posX_6 = this.transform.position.x;
+                puzzleData.posY_6 = this.transform.position.y;
             break;
 
             case 7:
-                //puzzleData.posX_7 = this.transform.position.x;
-                //puzzleData.posZ_7 = this.transform.position.z;
+                puzzleData.posX_7 = this.transform.position.x;
+                puzzleData.posY_7 = this.transform.position.y;
             break;
 
             case 8:
-                //puzzleData.posX_8 = this.transform.position.x;
-                //puzzleData.posZ_8 = this.transform.position.z;
+                puzzleData.posX_8 = this.transform.position.x;
+                puzzleData.posY_8 = this.transform.position.y;
             break;
 
             case 9:
-                //puzzleData.posX_9 = this.transform.position.x;
-                //puzzleData.posZ_9 = this.transform.position.z;
+                puzzleData.posX_9 = this.transform.position.x;
+                puzzleData.posY_9 = this.transform.position.y;
             break;
 
             case 10:
-                //puzzleData.posX_10 = this.transform.position.x;
-                //puzzleData.posZ_10 = this.transform.position.z;
+                puzzleData.posX_10 = this.transform.position.x;
+                puzzleData.posY_10 = this.transform.position.y;
             break;
 
             case 11:
-                //puzzleData.posX_11 = this.transform.position.x;
-                //puzzleData.posZ_11 = this.transform.position.z;
+                puzzleData.posX_11 = this.transform.position.x;
+                puzzleData.posY_11 = this.transform.position.y;
             break;
 
             case 12:
-                //puzzleData.posX_12 = this.transform.position.x;
-                //puzzleData.posZ_12 = this.transform.position.z;
+                puzzleData.posX_12 = this.transform.position.x;
+                puzzleData.posY_12 = this.transform.position.y;
             break;
 
             case 13:
-                //puzzleData.posX_13 = this.transform.position.x;
-                //puzzleData.posZ_13 = this.transform.position.z;
+                puzzleData.posX_13 = this.transform.position.x;
+                puzzleData.posY_13 = this.transform.position.y;
             break;
 
             case 14:
-                //puzzleData.posX_14 = this.transform.position.x;
-                //puzzleData.posZ_14 = this.transform.position.z;
+                puzzleData.posX_14 = this.transform.position.x;
+                puzzleData.posY_14 = this.transform.position.y;
             break;
 
             case 15:
-                //puzzleData.posX_15 = this.transform.position.x;
-                //puzzleData.posZ_15 = this.transform.position.z;
+                puzzleData.posX_15 = this.transform.position.x;
+                puzzleData.posY_15 = this.transform.position.y;
             break;
         }
     }
 
     public void SortPuzzles()
     {
-        //this.transform.position = new Vector3 (pointX, 0, pointZ);
+        //this.transform.position = new Vector2 (pointX, pointY);
     }
 
     public void ActivateInRightPositionAction()
