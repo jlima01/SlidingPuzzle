b09c018ff6428fa63e6f41a960c43263c345e05c
jlima01 <jonatasflima01@gmail.com>
2022-01-24 20:53:22 -0300

    Adição de Script e edição do menu.

---
+++ SlidingPuzzle/Assets/Scripts/UIScripts/ButtonHighlight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHighlight : MonoBehaviour, ISelectHandler , IPointerEnterHandler
+{
+  protected Button button;
+  
+  public UnityEvent OnHighlight, OnExitHighlight;
+
+  void Start()
+  {
+    button = GetComponent<Button>();
+  }
+
+  public void OnPointerEnter(PointerEventData eventData)
+  {
+    if(button.interactable == true)
+    {
+      ActivateHLAction();
+    }
+  }
+  
+  public void OnSelect(BaseEventData eventData)
+  {
+  
+  }
+  public void ActivateHLAction()
+  {
+    if(OnHighlight != null)
+      {
+        OnHighlight.Invoke();
+      }
+
+      StartCoroutine(DeactivateAction());
+  }
+  public void DeactivateHLAction()
+  {
+    if(OnExitHighlight != null)
+      {
+        OnExitHighlight.Invoke();
+      }
+  }
+  IEnumerator DeactivateAction()
+  {
+    yield return new WaitForSeconds(15);
+    DeactivateHLAction();
+  }
+}
